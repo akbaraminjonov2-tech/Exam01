4f543b69e5f2eb91864e2733f9fbf8165bfd4cae
akbaraminjonov2-tech <akbaraminjonov2@gmail.com>
2026-06-12 19:15:59 +0500

    task14.py bajarildi

---
+++ task14.py
@@ -0,0 +1,13 @@
+fayl = input("Fayl nomini kiriting: ")
+
+if fayl.endswith(".pdf"):
+    print("Fayl turi: pdf")
+
+elif fayl.endswith(".docx"):
+    print("Fayl turi: docx")
+
+elif fayl.endswith(".txt"):
+    print("Fayl turi: txt")
+
+else:
+    print("Noma'lum fayl turi")
