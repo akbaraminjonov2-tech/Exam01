aca031246903a4189483871372fb5a9e11786df3
akbaraminjonov2-tech <akbaraminjonov2@gmail.com>
2026-06-12 19:19:38 +0500

    task15.py bajarildi

---
+++ task15.py
@@ -0,0 +1,8 @@
+number = 30
+
+if number % 2 == 0:
+    print("30 soni 2 ga bo'linadi")
+if number % 3 == 0:
+    print("30 soni 3 ga bo'linadi")
+if number % 5 == 0:
+    print("30 soni 5 ga bo'linadi")
